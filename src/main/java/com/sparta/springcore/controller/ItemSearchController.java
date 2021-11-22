10f43aac698d6daba569c08b48e07fe34f96a153
uriseozz <iyoule@naver.com>
2021-11-22 23:05:55 +0900

    spring 심화강의 1주차

---
+++ src/main/java/com/sparta/springcore/controller/ItemSearchController.java
@@ -0,0 +1,31 @@
+package com.sparta.springcore.controller;
+
+import com.sparta.springcore.dto.ItemDto;
+import com.sparta.springcore.service.ItemSearchService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.io.IOException;
+import java.util.List;
+
+@Controller
+public class ItemSearchController {
+
+    private final ItemSearchService itemSearchService;
+
+    @Autowired
+    public ItemSearchController(ItemSearchService itemSearchService) {
+        this.itemSearchService = itemSearchService;
+    }
+
+    @GetMapping("/api/search")
+    @ResponseBody
+    public List<ItemDto> getItems(@RequestParam String query) throws IOException {
+        List<ItemDto> itemDtoList = itemSearchService.getItems(query);
+
+        return itemDtoList;
+    }
+}
