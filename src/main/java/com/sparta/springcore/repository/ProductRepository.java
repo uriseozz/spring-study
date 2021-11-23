336a711cf983e3924d18d35ee02f9c2bd7d31613
uriseozz <iyoule@naver.com>
2021-11-23 15:11:28 +0900

    회원가입/로그인 페이지 구현

--- src/main/java/com/sparta/springcore/repository/ProductRepository.java
+++ src/main/java/com/sparta/springcore/repository/ProductRepository.java
@@ -3,4 +3,8 @@ package com.sparta.springcore.repository;
 import com.sparta.springcore.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProductRepository extends JpaRepository<Product, Long> { }
+import java.util.List;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    List<Product> findAllByUserId(Long userId);
+}
