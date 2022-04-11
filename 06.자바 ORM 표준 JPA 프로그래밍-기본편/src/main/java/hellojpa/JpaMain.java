210879892496f7a683e609abdbc848b9bf49a440
uriseozz <iyoule@naver.com>
2022-04-11 20:14:24 +0900

    영속성관리, 엔티티 매핑

--- 06.자바 ORM 표준 JPA 프로그래밍-기본편/src/main/java/hellojpa/JpaMain.java
+++ 06.자바 ORM 표준 JPA 프로그래밍-기본편/src/main/java/hellojpa/JpaMain.java
@@ -2,6 +2,7 @@ package hellojpa;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
 public class JpaMain {
@@ -9,9 +10,43 @@ public class JpaMain {
     public static void main(String[] args) {
         EntityManagerFactory emf =  Persistence.createEntityManagerFactory("hello");
 
-        EntityManager entityManager = emf.createEntityManager();
+        // 엔티티 등록
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
 
-        entityManager.close();
+        //엔티티 매니저는 데이터 변경시 트랜잭션을 시작
+        tx.begin(); // [트랜잭션] 시작
+
+
+        try {
+
+            // 비영속
+            Member member = new Member();
+            member.setId(2L);
+            member.setName("HelloB");
+
+            //영속 : DB저장 X, 1차 캐시에 저장
+            em.persist(member);
+
+            // 1차캐시에서 조회
+            Member findMember = em.find(Member.class, "member1");
+
+            // DB에서 조회
+            Member findMember2 = em.find(Member.class, "member2");
+
+            em.persist(findMember);
+            em.persist(findMember2);
+            //여기까지 INSERT SQL을 데이터베이스에 보내지 않음
+            // 커밋하는 순간 데이터베이스에 INSERT SQL을 보낸다.
+            tx.commit(); // [트랜잭션] 커밋
+
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+        em.close();
 
         emf.close();
     }
--- 06.자바 ORM 표준 JPA 프로그래밍-기본편/src/main/java/hellojpa/Member.java
+++ 06.자바 ORM 표준 JPA 프로그래밍-기본편/src/main/java/hellojpa/Member.java
@@ -1,2 +1,28 @@
-package hellojpa;public class Member {
+package hellojpa;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "USER")
+public class Member {
+    @Id
+    private Long id;
+
+    @Column(name = "username")
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
